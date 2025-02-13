eef6d1d7318e3a90e3b4e9324fea9a5bd81493da
Gaurav Sanjiv Upadhyay <53818928+GauravUp28@users.noreply.github.com>
2025-02-12 18:53:01 -0800

    Create Main.java

---
+++ src/Minimum_Operations_to_Exceed_Threshold_Value_II_3066/Main.java
@@ -0,0 +1,11 @@
+package Minimum_Operations_to_Exceed_Threshold_Value_II_3066;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int[] nums = new int[] {2,11,10,1,3};
+
+        System.out.println("Minimum Operations: " + solution.minOperations(nums, 10));
+    }
+}
